b93b9d7cfabe9b4dc710575adc8cebfb69728231
Leo Allen <leo.f.allen@gmail.com>
2013-01-27 15:38:25 +0000

    Initial commit, adding rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poker::Application.config.secret_token = '29a808dd6a3c6acaa150a623770aff5c8d6e9cbe747e8e221d4fceccabcdb848c1a83b13f5c07315733990da2b2a4f7ca667587a32abbea3ff5f270def12987f'
